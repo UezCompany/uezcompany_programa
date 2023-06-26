fce5538fe3a1ea225a4ebb9d457505c1bf5359a6
Renato Gomes do Nascimento <110681726+RENATO-GDN@users.noreply.github.com>
2023-06-26 15:37:42 -0300

    Initial commit

---
+++ src/uezcompanyprograma/UEZCompanyPrograma.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uezcompanyprograma;
+
+/**
+ *
+ * @author renat
+ */
+public class UEZCompanyPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
